4ca6de53c224abc96ceb9b3ba7fe1c17ab7280c5
Co2333 <2333OwO@gmail.com>
2018-10-01 14:45:08 +0800

    add a helper tool

---
+++ rootHandler/checkMyAss.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#  checkMyAss.sh
+#  IPAForce
+#
+#  Created by Lakr Sakura on 2018/10/1.
+#  Copyright © 2018 Lakr Sakura. All rights reserved.
+
+# 获取当前目录
+BASEDIR=$(dirname "$0")
+
+# 检查 py 工具是否存在
+if [ -e $BASEDIR/SMJobBlessUtil.py ]
+then
+echo "[*] SMJobBlessUtil.py found! Ready to check your ass."
+else
+echo "[Panic] SMJobBlessUtil.py is not here my friend."
+fi
+
+# 开始修正
+./SMJobBlessUtil.py setreq   ./../Build/Products/Debug/IPAForce.app   ./../IPAForce/Info.plist   ./rootHandler-Info.plist
+
+# 检查一下
+# ./SMJobBlessUtil.py check   ./../Build/Products/Debug/IPAForce.app
+
+# exit 0
--- rootHandler/main.c
+++ rootHandler/main.c
@@ -1,17 +1,15 @@
 //
-//  main.m
+//  main.c
 //  rootHandler
 //
 //  Created by Lakr Sakura on 2018/10/1.
 //  Copyright © 2018 Lakr Sakura. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    // insert code here...
+    printf("Hello, World!\n");
     return 0;
 }
