b65d6dbdb6da085128cec9bca11a95658ca10ef9
Co2333 <2333OwO@gmail.com>
2018-10-03 10:32:07 +0800

    Fixed can't init documents
    Fixed #3

--- IPAForce/Codes/AppDelegate.m
+++ IPAForce/Codes/AppDelegate.m
@@ -17,22 +17,9 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
     
-    // 创建存档目录
-    NSURL *tempDir = [[NSFileManager defaultManager] temporaryDirectory];
-    NSURL *savesDir = [tempDir URLByAppendingPathComponent:(@"Saves") isDirectory:true];
-    BOOL fuckThis = YES;
-    if (![[NSFileManager defaultManager] fileExistsAtPath:savesDir.path isDirectory:&fuckThis]) {
-        [[NSFileManager defaultManager] createDirectoryAtPath:savesDir.path withIntermediateDirectories:YES attributes:NULL error:NULL];
-    }
+
     
-    NSURL *sshAddrSave = [[[NSFileManager defaultManager] temporaryDirectory] URLByAppendingPathComponent:@"Saves/sshAddress.txt"];
-    NSString *inputString = [[NSString alloc] initWithContentsOfFile:sshAddrSave.path
-                                                            encoding:NSUTF8StringEncoding
-                                                               error:NULL];
-    if (inputString == nil) {
-        inputString = @"0.0.0.0:22";
-    }
-    [inputString writeToURL:sshAddrSave atomically:YES encoding:NSUTF8StringEncoding error:NULL];
+    return;
 }
     
 
